f0de565a2e33e91a12b5fdf09bfd16024d4060f5
Yevhen Kalyna <yevhen.kalyna@gmail.com>
2026-03-04 08:45:40 +0200

    fix: Handle ValueError in upcoming birthday calculation in AddressBook

--- topic_10/task1.py
+++ topic_10/task1.py
@@ -103,7 +103,12 @@ class AddressBook(UserDict):
             if record.birthday is None:
                 continue
 
-            birthday_this_year = record.birthday.value.replace(year=today.year)
+            try:
+                birthday_this_year = record.birthday.value.replace(year=today.year)
+            except ValueError:
+                birthday_this_year = record.birthday.value.replace(
+                    year=today.year, month=3, day=1
+                )
 
             if birthday_this_year < today:
                 birthday_this_year = birthday_this_year.replace(year=today.year + 1)
